e66760793bee78995935d4ca70e1038e7d0f8f99
tommiluk <sakari.lukkarinen@metropolia.fi>
2024-12-17 20:04:23 +0200

    modified:   osa1/src/App.jsx

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
 
   return (
     <div>
-      <Header name='Half Stack application development'/>
+      <Header name={course} />
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
