eef7bdc459eca883a2fafaa2e292866a564dd092
tommiluk <sakari.lukkarinen@metropolia.fi>
2024-12-17 20:03:09 +0200

    modified:   osa1/src/App.jsx

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -6,10 +6,10 @@ const Header = (props) => {
   )
 }
 
-const Hello = () => {
+const Content = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>{props.part} {props.exercises}</p>
     </div>
   )
 }
@@ -26,17 +26,10 @@ const App = () => {
   return (
     <div>
       <Header name='Half Stack application development'/>
-      <Hello />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
 
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
