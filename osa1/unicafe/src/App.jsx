27872f44c66e7fb1a4e0e9433a127be4c1a73098
sakluk <sakari.lukkarinen@metropolia.fi>
2024-12-19 21:31:52 +0200

    modified:   osa1/unicafe/src/App.jsx 	modified:   osa1/unicafe/src/main.jsx

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,20 +1,14 @@
 import { useState } from 'react'
 
-// const App = () => {
-//   // tallenna napit omaan tilaansa
-//   const [good, setGood] = useState(0)
-//   const [neutral, setNeutral] = useState(0)
-//   const [bad, setBad] = useState(0)
+const App = () => {
 
-//   return (
-//     <div>
-//       code here
-//     </div>
-//   )
-// }
+  const [ counter, setCounter ] = useState(0)
 
-const App = (props) => {
-  const {counter} = props
+  setTimeout(
+    () => setCounter(counter + 1),
+    1500
+  )
+
   return (
     <div>{counter}</div>
   )
--- osa1/unicafe/src/main.jsx
+++ osa1/unicafe/src/main.jsx
@@ -1,21 +1,5 @@
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-let counter = 1
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <App counter={counter} />
-// )
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-const refresh = () => {
-    root.render(<App counter={counter} />)
-  }
-
-  setInterval(() => {
-    refresh()
-    counter += 1
-  }, 2000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
