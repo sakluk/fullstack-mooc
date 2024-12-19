52a9fbaa5fec646e8824f7a4be238e5d05e7669a
sakluk <sakari.lukkarinen@metropolia.fi>
2024-12-19 21:52:58 +0200

    modified:   osa1/unicafe/src/App.jsx

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,22 +1,43 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
   
   const [ counter, setCounter ] = useState(0)
+
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
+      <Display counter={counter}/>
 
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
