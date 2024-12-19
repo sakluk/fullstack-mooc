a0a72484b8bf7ba42ed3b55473911e845bace0f8
sakluk <sakari.lukkarinen@metropolia.fi>
2024-12-19 21:46:48 +0200

    modified:   osa1/unicafe/src/App.jsx

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 
 const App = () => {
-
-  const [ counter, setCounter ] = useState(0)
-
-  setTimeout(
-    () => setCounter(counter + 1),
-    1500
-  )
-
-  console.log('rendering...', counter)
   
+  const [ counter, setCounter ] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter}</div>
+
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+
+      <button onClick={setToZero}>
+        zero
+      </button>
+    </div>
   )
 }
 
