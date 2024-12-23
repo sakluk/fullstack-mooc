23b1e9b37addf0de63f7f0931e777194bd9b88f2
sakluk <sakari.lukkarinen@metropolia.fi>
2024-12-23 14:35:23 +0200

    2.4. kurssitiedot vaihe9:

--- osa2/kurssitiedot/README.md
+++ osa2/kurssitiedot/README.md
@@ -3,19 +3,20 @@
 Tässä kansiossa on esitetty palautukset tehtäviin
 - 2.1. kurssitiedot vaihe6: renderöinnin viimeistely
 - 2.2. kurssitiedot vaihe7: yhteenlaskettu tehtävien määrä
-- 2.3. kurssiteidot vaihe8: käytetty reduce metodia yhteenlaskuun
+- 2.3. kurssitiedot vaihe8: käytetty reduce metodia yhteenlaskuun
+- 2.4. kurssitiedot vaihe9: mielivaltainen määrä kursseja
 
 ## Käytetyt tunnit
   Tunnit | Tehtävä                 |
 | ------- | ---------------------- |
 | 1 | Tutustuttu [Kokoelmien renderöinti ja modulit](https://fullstackopen.com/osa2/kokoelmien_renderointi_ja_moduulit#kokoelmien-renderoiminen) osioon |
-| 1 | Tehty tehtävät 2.1 - 2.3
-| **1** | **Yhteensä** |
+| 1 | Tehty tehtävät 2.1 - 2.4
+| **2** | **Yhteensä** |
 
 ## Muistiinpanot
 - Luotu uusi React+Vite mallipohja, poistettu tarpeettomat tiedostot
 - Kopioitu [Kokoelmien renderöiminen](https://fullstackopen.com/osa2/kokoelmien_renderointi_ja_moduulit#kokoelmien-renderoiminen) main.jsx ja App.jsx ensimmäiseksi harjoittelupohjaksi
-- Tehty tehtävät 2.1 ja 2.2.
+- Tehty tehtävät 2.1 - 2.4
 
 ## Mallipohja
 
--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -35,42 +35,76 @@ const Total = (props) => {
   )
 }
 
-const Course = (props) => {
+const Course = ({ course }) => {
+  console.log('Course', course)
   return (
     <div>
-      <Header name={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+const Courses = ({ courses }) => {
+  // console.log('Courses', props) 
+  console.log(courses)
+  return (
+    <div>
+      {courses.map(course => <Course key={course.id} course={course} />)}
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Courses courses={courses} />
     </div>
   )
 }
