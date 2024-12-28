8783a98d55065cb17724d0afd5d6a39b64f6696b
sakluk <sakari.lukkarinen@metropolia.fi>
2024-12-28 16:15:58 +0200

    uibuild

--- osa3/notes-palvelin/index.js
+++ osa3/notes-palvelin/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -74,7 +77,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
